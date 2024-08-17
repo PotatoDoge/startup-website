df907fc28ff65f655e003cc8dc693278af896221
bprado <bastian.prado.v@gmail.com>
2024-08-17 01:39:10 -0700

    Change font

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,8 +6,7 @@ const Header = () => {
         <div className="header">
             <div className="header-contents">
                 <h2>Tech Solutions Group: Innovative IT Solutions</h2>
-                <p>Transforming ideas into innovative solutions. Specializing in custom software, websites, mobile apps, and IT infrastructure.</p>
-                <button>View menu</button>
+                <p>Transforming ideas into innovative solutions. Specializing in custom software, websites, desktop apps, mobile apps, and IT infrastructure.</p>
             </div>
         </div>
     )
