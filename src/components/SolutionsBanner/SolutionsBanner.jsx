bbd2eaf141b22a3b933f1827ad5df33bdca366cf
bprado <bastian.prado.v@gmail.com>
2024-08-16 22:34:48 -0700

    Add solutions banner component

---
+++ src/components/SolutionsBanner/SolutionsBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import code from '../../assets/code.jpg'
+import './SolutionsBanner.css'
+
+const SolutionsBanner = () => {
+    return (
+        <div className="solutions-banner"></div>
+    )
+}
+
+export default SolutionsBanner;
